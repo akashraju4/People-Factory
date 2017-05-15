f7289128b5be290396f0e808194714f0c65a3a0c
Akash <akash.raju@outlook.com>
2017-05-15 15:11:38 -0400

    Day 1 HW w/out extra credit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const personForm = document.querySelector('form')
+const personForm = document.querySelectorAll('form')[0]
 
 const handleSubmit = (ev) => {
     ev.preventDefault()
     const form = ev.target
     const details = document.querySelector('.details')
+    
+    // for(i = 0; i < personForm.length; i++)
+    // {
+    //     personForm[i].value = document.createElement()
+    //     personForm[i].textContent = personForm[i].value
+    //     details.appendChild(personForm[i])
+    // }
 
     const personName = form.personName.value
     const hairColor = form.hairColor.value
@@ -11,26 +18,42 @@ const handleSubmit = (ev) => {
     const birthPlace = form.birthPlace.value
 
 
-    const colorDiv = `
-    <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>`
+    // const colorDiv = `
+    // <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>`
 
+   //details.innerHTML = `<em>${personName}</em>`
+    const breakline = document.createElement('br');
+    const name = document.createElement('name')
+    name.textContent = 'Name:' + personName
+    details.appendChild(name)
+    details.appendChild(breakline)
+    const hair = document.createElement('hair')
+    hair.textContent = "Hair Color:"
+    const colors = document.createElement('div')
+    colors.style = `height: 50px; width: 100px; background-color: ${hairColor}`
+    details.appendChild(hair)
+    details.appendChild(colors)
+    const breakline3 = document.createElement('br');
+    const years = document.createElement('years')
+    years.textContent = 'Age:' + age
+    details.appendChild(years)
+    details.appendChild(breakline3)
+    const origin = document.createElement('origin')
+    const breakline4 = document.createElement('br');
+    origin.textContent = 'Birth Place:' + birthPlace
+    details.appendChild(origin)
+    details.appendChild(breakline4)
 
-    // details.innerHTML = `<em>${personName}</em>`
-    
-    //const em = document.createElement('em')
-    //em.textContent = subject + ':' + personName
-    //details.appendChild(em)
-    
     //one way to show type
 
-    details.innerHTML = `
-        <ul>
-        <li>Name: ${personName}</li>
-        <li>Hair Color: ${colorDiv}</li>
-        <li>Age: ${age}</li>
-        <li>Birthplace: ${birthPlace}</li>
-        </ul>
-        `
+   // details.innerHTML = `
+     //   <ul>
+     //   <li>Name: ${personName}</li>
+     //   <li>Hair Color: ${colorDiv}</li>
+     //   <li>Age: ${age}</li>
+     //  <li>Birthplace: ${birthPlace}</li>
+     //    </ul>
+        //`
     
     
 }
